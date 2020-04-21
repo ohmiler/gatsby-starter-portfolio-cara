85235feda88bf12367a8ec7a54200732a59cd73e
Patiphan Phengpao <misterpatipan123@gmail.com>
2020-04-21 12:32:32 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `MilerDev Official`,
   },
   plugins: [
     {
